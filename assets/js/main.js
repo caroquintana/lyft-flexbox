def53d13d51b38c7452f24e9e4b388a7057b9369
caroquintana <caro.quintana.n@gmail.com>
2017-06-04 17:37:36 -0400

    avance js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,26 @@
-//traer los dos nav, el visible y el oculto//
-var primernav= document.getElementById('menuprincipal');
-var segundonav = document.getElementById('menuprincipalg');
+/*
+*/
 
-var limite = 0;
+var start = 0;
 
 
-function scrollFunction() {
-    var posicioninicial = document.documentElement.scrollTop || document.body.scrollTop;
-	var posicionfinal = document.documentElement.scrollTop = document.body.scrollTop = 1000;
-    if (posicionfinal > posicioninicial) {
-    	alert("Me desplazo");
-    }
-    
+window.addEventListener("scroll", eventoScroll, false);
+
+
+function eventoScroll() {
+	var scroll = window.pageYOffset || document.body.scrollTop;
+	if (scroll > start) {
+		document.getElementById("menuprincipalg").classList.add("sinbg");
+		document.getElementById("logo").src = "../lyft-flexbox/assets/images/logo-pink.png";
+	}else{
+		document.getElementById("menuprincipalg").classList.remove("sinbg");
+		document.getElementById("logo").src = "../lyft-flexbox/assets/images/logo-white.png";
+	} 
+	start = scroll;
 }
 
-window.onscroll = scrollFunction;
+
+
+
+
+
